8cbd66c06e2e908e094ae8956033b3a29d890f32
SisiphoNgamlana <watziznaim@gmail.com>
2022-05-10 09:41:58 +0200

    Add mockito dependency. Add presenter Unit tests.

---
+++ app/src/test/java/com/example/loginactivity/PresenterTests.java
@@ -0,0 +1,100 @@
+package com.example.loginactivity;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import android.text.TextUtils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PresenterTests {
+
+    LoginActivityMVP.Model mockLoginModel;
+    LoginActivityMVP.View mockLoginView;
+    LoginActivityPresenter presenter;
+    User user;
+
+    @Before
+    public void setup(){
+
+        mockLoginModel = mock(LoginActivityMVP.Model.class);
+        mockLoginView = mock(LoginActivityMVP.View.class);
+        user = new User("Fox", "Mulder");
+
+        presenter = new LoginActivityPresenter(mockLoginModel);
+        presenter.setView(mockLoginView);
+
+    }
+
+    //TODO : Test fails, rename this
+    @Test
+    public void noInteractionWithView(){
+        when(mockLoginModel.getUser()).thenReturn(null);
+
+        presenter.getCurrentUser();
+
+        verifyZeroInteractions(mockLoginView);
+    }
+
+    @Test
+    public void loadUserFromRepository() {
+        when(mockLoginModel.getUser()).thenReturn(user);
+
+        presenter.getCurrentUser();
+        verify(mockLoginModel,times(1)).getUser();
+        verify(mockLoginView,times(1)).setFirstName("Fox");
+        verify(mockLoginView,times(1)).setLastName("Mulder");
+        verify(mockLoginView,never()).showUserNotAvailable();
+
+    }
+
+    @Test
+    public void testShowUserNotAvailable() {
+        when(mockLoginModel.getUser()).thenReturn(null);
+
+        presenter.getCurrentUser();
+        verify(mockLoginView,never()).setFirstName("Fox");
+        verify(mockLoginView,never()).setLastName("Mulder");
+        verify(mockLoginView,times(1)).showUserNotAvailable();
+
+    }
+
+    @Test
+    public void testShowErrorMessageEmptyFields() {
+
+        when(mockLoginView.getFirstName()).thenReturn(null);
+
+        presenter.loginButtonClicked();
+
+        verify(mockLoginView,times(1)).getFirstName();
+        verify(mockLoginView,never()).getLastName();
+        verify(mockLoginView,times(1)).showInputError();
+
+        when(mockLoginView.getFirstName()).thenReturn("Fox");
+        when(mockLoginView.getLastName()).thenReturn(null);
+
+        presenter.loginButtonClicked();
+
+        verify(mockLoginView,times(2)).getFirstName();
+        verify(mockLoginView,times(1)).getLastName();
+        verify(mockLoginView,times(2)).showInputError();
+
+    }
+
+    @Test
+    public void testShowSuccessMessageOnSaveSuccess() {
+
+        when(mockLoginView.getFirstName()).thenReturn("Fox");
+        when(mockLoginView.getLastName()).thenReturn("Mulder");
+
+        presenter.loginButtonClicked();
+
+        verify(mockLoginView,times(1)).showUserSavedMessage();
+
+    }
+}
