16257086a3e3650052b75bb24a2be6c7f96ff5cf
SisiphoNgamlana <watziznaim@gmail.com>
2022-05-12 13:42:40 +0200

    Add service call to the mainactivity, add call using retrofit, add call using RxJava

--- app/src/main/java/com/example/loginactivity/LoginActivity.java
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -10,12 +10,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.loginactivity.api.TwitchAPI;
+import com.example.loginactivity.api.apimodel.Top;
+import com.example.loginactivity.api.apimodel.Twitch;
+
+import java.util.List;
+
 import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class LoginActivity extends AppCompatActivity implements LoginActivityMVP.View {
 
     @Inject
     LoginActivityMVP.Presenter presenter;
+
+    @Inject
+    TwitchAPI twitchAPI;
 
     EditText firstNameEditText;
     EditText lastNameEditText;
@@ -27,6 +45,55 @@ public class LoginActivity extends AppCompatActivity implements LoginActivityMVP
         setContentView(R.layout.activity_main);
 
         ((App) getApplication()).getApplicationComponent().inject(this);
+
+        /*
+        This is the service call without RxJava.
+        Call<Twitch> call = twitchAPI.getTopGames("Some header here");
+        call.enqueue(new Callback<Twitch>() {
+            @Override
+            public void onResponse(Call<Twitch> call, Response<Twitch> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<Top> gamesList = response.body().getTop();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Twitch> call, Throwable t) {
+                //TODO : Show network failure message
+                t.printStackTrace();
+            }
+        });*/
+
+        //This is the API call using RxJava
+        twitchAPI.getTopGamesObservable("HeaderString")
+                .flatMap(new Func1<Twitch, Observable<Top>>() {
+                    @Override
+                    public Observable<Top> call(Twitch twitch) {
+                        return Observable.from(twitch.getTop());
+                    }
+                }).flatMap(new Func1<Top, Observable<String>>() {
+            @Override
+            public Observable<String> call(Top top) {
+                return Observable.just(top.getGame().getName());
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+
+                    }
+                });
 
         firstNameEditText = findViewById(R.id.first_name_edittext);
         lastNameEditText = findViewById(R.id.last_name_edittext);
